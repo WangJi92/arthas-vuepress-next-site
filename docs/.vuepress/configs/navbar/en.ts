60e440ad7e4cccbaf2d79f921747c00bd0f5ab7a
wangji <983433479@qq.com>
2022-05-08 19:40:38 +0800

    commit

---
+++ docs/.vuepress/configs/navbar/en.ts
@@ -0,0 +1,19 @@
+import type { NavbarConfig } from '@vuepress/theme-default'
+
+export const en: NavbarConfig = [
+    {
+        text: 'HOME PAGE',
+        link: '/en/',
+    },
+    {
+        text: 'DOCS',
+        link: '/en/',
+    },
+    {
+        text: 'DOWNLOAD',
+        link: '/en/download',
+    },
+    {
+        text: 'COMMUNITY',
+        link: 'https://github.com/alibaba/arthas/issues',
+    }]
---
+++ docs/.vuepress/configs/navbar/zh.ts
@@ -0,0 +1,20 @@
+import type { NavbarConfig } from '@vuepress/theme-default'
+
+export const zh: NavbarConfig = [
+    {
+        text: '首页',
+        link: '/zh/',
+    },
+    {
+        text: '文档',
+        link: '/zh/',
+    },
+    {
+        text: '下载',
+        link: '/zh/download',
+    },
+    {
+        text: '社区',
+        link: 'https://github.com/alibaba/arthas/issues',
+    },
+]
